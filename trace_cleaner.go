fc025805c8a43d5fc5b34068ced92747a740668b
Martin Multhaupt <m.multhaupt@setlog.com>
2020-01-31 16:30:24 +0100

    Clean up trace cleaner

--- trace_cleaner.go
+++ trace_cleaner.go
@@ -6,44 +6,51 @@ import (
 	"strings"
 )
 
-var verboseRegExps []*regexp.Regexp = []*regexp.Regexp{
+type traceCleaner struct {
+	destination    io.Writer
+	buffer         string
+	removeNextLine bool
+}
+
+func (tc *traceCleaner) Write(p []byte) (n int, err error) {
+	tc.buffer += string(p)
+	for {
+		nextLineIndex := strings.Index(tc.buffer, "\n") + 1
+		if nextLineIndex == 0 {
+			return len(p), nil
+		}
+		line := tc.buffer[:nextLineIndex]
+		tc.buffer = tc.buffer[nextLineIndex:]
+		if tc.removeNextLine {
+			n += len(line)
+			tc.removeNextLine = false
+			continue
+		}
+
+		if isUnwantedLine(line) {
+			n += len(line)
+			tc.removeNextLine = true
+		} else {
+			written, err := tc.destination.Write([]byte(line))
+			n += written
+			if err != nil {
+				return n, err
+			}
+		}
+	}
+}
+
+var unwantedLineRegExps []*regexp.Regexp = []*regexp.Regexp{
 	regexp.MustCompile(`^panic\(.*$`),
 	regexp.MustCompile(`^runtime/debug.Stack\(.*$`),
 	regexp.MustCompile(`^github.com/setlog/panik\..*\(.*$`),
 }
 
-type traceCleaner struct {
-	destination io.Writer
-	line        string
-	removeNext  bool
-}
-
-func (tc *traceCleaner) Write(p []byte) (n int, err error) {
-	tc.line += string(p)
-OUTER:
-	for {
-		nextLineIndex := strings.Index(tc.line, "\n") + 1
-		if nextLineIndex == 0 {
-			return len(p), nil
-		}
-		line := tc.line[:nextLineIndex]
-		tc.line = tc.line[nextLineIndex:]
-		if tc.removeNext {
-			n += len(line)
-			tc.removeNext = false
-			continue
-		}
-		for _, verboseRegExp := range verboseRegExps {
-			if verboseRegExp.MatchString(line[:len(line)-1]) {
-				n += len(line)
-				tc.removeNext = true
-				continue OUTER
-			}
-		}
-		written, err := tc.destination.Write([]byte(line))
-		n += written
-		if err != nil {
-			return n, err
+func isUnwantedLine(line string) bool {
+	for _, verboseRegExp := range unwantedLineRegExps {
+		if verboseRegExp.MatchString(line[:len(line)-1]) {
+			return true
 		}
 	}
+	return false
 }
